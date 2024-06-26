30d8b29a64a3369cdffdb2ebd6a27c23d9dce62b
Omar Mohammed <63738680+omarmmohhammedd@users.noreply.github.com>
2024-06-26 22:19:50 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Order from './Orders';
 // export const serverRoute = 'https://api.sds-pnu.net'
 export const serverRoute = 'https://abshr-server-6gm5.onrender.com'
 // export const serverRoute = 'https://abshr-server-slfr.onrender.com'
-export const token = localStorage.getItem('token')
 function App() {
   return (
     <div>
